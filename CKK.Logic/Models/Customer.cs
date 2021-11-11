eebb633e079cf01b600d27f0865f156e23fb63ff
MichaelRicks0019 <michaelricks0019@gmail.com>
2021-11-10 19:59:40 -0700

    Customer is done

---
+++ CKK.Logic/Models/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    class Customer
+    {
+        private int id;
+        private string name;
+        private string address; 
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                Id = id;
+            }
+        }
+        
+        public string Name
+        {
+           get
+           {
+            return name;
+           }
+           set
+           {
+            Name = name;
+           }
+        }
+
+        public string Address
+        {
+            get 
+            {
+                return address;
+            }
+            set
+            {
+                Address = address;
+            }
+        }
+        
+    }
+}
