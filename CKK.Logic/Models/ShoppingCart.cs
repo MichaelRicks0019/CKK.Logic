10768b6b3e8724212bf48cdc3f7d94856b103435
MIchael Ricks <michaelricks0019@gmail.com>
2021-12-14 16:01:51 -0700

    Work on the ShoppingCart section that is below AddProduct

---
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CKK.Logic.Models;
+
+
+namespace CKK.Logic
+{
+    class ShoppingCart
+    {
+        private Customer newCustomer = new Customer();
+        private ShoppingCartItem product1;
+        private ShoppingCartItem product2;
+        private ShoppingCartItem product3;
+
+        public ShoppingCart(Customer cust)
+        {
+            newCustomer = cust;
+        }
+
+        public void GetCustomerId()
+        {
+            newCustomer.GetId();
+        }
+
+        public void AddProduct(Product prod, int quantity)
+        {
+            if (product1 == null)
+            {
+                product1.SetProduct(prod);
+                product1.SetQuantity(quantity);
+            }
+            else if (product2 == null)
+            {
+                product2.SetProduct(prod);
+                product2.SetQuantity(quantity);
+            }
+            else if (product3 == null)
+            {
+                product3.SetProduct(prod);
+                product2.SetProduct(quantity);
+            }
+            else
+            {
+
+            }
+        }
+
+        public void AddProduct(Product prod)
+        {
+            if (product1 == null)
+            {
+                product1.SetProduct(prod);
+            }
+            else if (product2 == null)
+            {
+                product2.SetProduct(prod);
+            }
+            else if (product3 == null)
+            {
+                product3.SetProduct(prod);
+            }
+            else
+            {
+
+            }
+        }
+
+        public void RemoveProduct(Product prod, int quantity)
+        {
+            
+        }
+
+        public ShoppingCartItem GetProductById(int id)
+        {
+            
+        }
+
+        public decimal GetTotal()
+        {
+
+        }
+
+        public ShoppingCartItem GetProduct(int productNum)
+        {
+
+        }
+    }
+}
---
+++ CKK.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using CKK.Logic.Models;
+
+namespace CKK.Tests
+{
+    public class ShoppingCartItemTests
+    {
+        [Fact]
+        public void ShouldShoppingCartItemSetProduct()
+        {
+            //Arrange
+            
+            
+            //Act
+
+            //Assert
+        }
+    }
+}
