719c7a896a87dff1b9514539bfce1705dc61e376
MIchael Ricks <michaelricks0019@gmail.com>
2021-12-15 20:05:26 -0700

    Work on Unit Tests

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -30,17 +30,26 @@ namespace CKK.Logic
             if (product1 == null)
             {
                 product1.SetProduct(prod);
-                product1.SetQuantity(quantity);
+                if (quantity >= 0)
+                {
+                    product1.SetQuantity(quantity);
+                }
             }
             else if (product2 == null)
             {
                 product2.SetProduct(prod);
-                product2.SetQuantity(quantity);
+                if (quantity >= 0)
+                {
+                    product2.SetQuantity(quantity);
+                }
             }
             else if (product3 == null)
             {
                 product3.SetProduct(prod);
-                product2.SetProduct(quantity);
+                if (quantity >= 0)
+                {
+                    product2.SetQuantity(quantity);
+                }
             }
             else
             {
@@ -48,43 +57,125 @@ namespace CKK.Logic
             }
         }
 
-        public void AddProduct(Product prod)
+        public ShoppingCartItem AddProduct(Product prod)
         {
             if (product1 == null)
             {
                 product1.SetProduct(prod);
+                product1.SetQuantity(1);
+                return product1;
             }
             else if (product2 == null)
             {
                 product2.SetProduct(prod);
+                product2.SetQuantity(1);
+                return product2;
+
             }
             else if (product3 == null)
             {
                 product3.SetProduct(prod);
+                product3.SetQuantity(1);
+                return product3;
             }
             else
             {
-
+                return null;
             }
         }
 
-        public void RemoveProduct(Product prod, int quantity)
+        public ShoppingCartItem RemoveProduct(Product prod, int quantity)
         {
-            
+            if(product1 != null && product1.GetProduct().GetId() == prod.GetId())
+            {
+                int qty = product1.GetQuantity();
+                if (quantity < qty)
+                {
+                    product1.SetQuantity(qty - quantity);
+                    return product1;
+                }
+                else
+                {
+                    product1 = null;
+                    return product1;
+                }
+            }
+            else if (product2 != null && product2.GetProduct().GetId() == prod.GetId())
+            {
+                int qty = product2.GetQuantity();
+                if (quantity < qty)
+                {
+                    product2.SetQuantity(qty - quantity);
+                    return product2;
+                }
+                else
+                {
+                    product2 = null;
+                    return product2;
+                }
+            }
+            else if (product3 != null && product3.GetProduct().GetId() == prod.GetId())
+            {
+                int qty1 = product3.GetQuantity();
+                if (quantity < qty1)
+                {
+                    product3.SetQuantity(qty1 - quantity);
+                    return product3;
+                }
+                else
+                {
+                    product3 = null;
+                    return product3;
+                }
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public ShoppingCartItem GetProductById(int id)
         {
-            
+            if (product1.GetProduct().GetId() == id)
+            {
+                return product1;
+            }
+            else if (product2.GetProduct().GetId() == id)
+            {
+                return product2;
+            }
+            else if (product3.GetProduct().GetId() == id)
+            {
+                return product3;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public decimal GetTotal()
         {
-
+            decimal total1 = product1.GetQuantity() * product1.GetProduct().GetPrice();
+            decimal total2 = product2.GetQuantity() * product2.GetProduct().GetPrice();
+            decimal total3 = product3.GetQuantity() * product3.GetProduct().GetPrice();
+            decimal total = total1 + total2 + total3;
+            return total;
         }
 
         public ShoppingCartItem GetProduct(int productNum)
         {
+            switch (productNum)
+            {
+                case 1:
+                    return product1;
+                case 2:
+                    return product2;
+                case 3:
+                    return product3;
+                default:
+                    return null;
+            }
 
         }
     }
--- CKK.Tests/UnitTest1.cs
+++ CKK.Tests/UnitTest1.cs
@@ -1,20 +1,34 @@
 using System;
 using Xunit;
 using CKK.Logic.Models;
+using CKK.Logic;
+
 
 namespace CKK.Tests
 {
     public class ShoppingCartItemTests
     {
         [Fact]
-        public void ShouldShoppingCartItemSetProduct()
+        public void AddProductCheese_ShoppingCartItem()
         {
+            var id = 6786;
+            var name = "Cheese";
+            var price = 4.73m;
             //Arrange
-            
-            
+            var product1 = new Product();
+            product1.SetId(id);
+            product1.SetName(name);
+            product1.SetPrice(price);
+
+            var cheese = new StoreItem(product1, 10);
+            cheese.SetProduct(product1);
+
+
             //Act
+           
 
             //Assert
+            Assert.Equal(product1, Store.GetStoreItem(1);
         }
     }
 }
