1cd833423399b00261627e823014775995e55a17
MIchael Ricks <michaelricks0019@gmail.com>
2022-12-19 20:19:53 -0700

    save

---
+++ CoreysKnickKnacks/Windows/AddItemWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CKK.Logic.Exceptions;
+using CKK.Logic.Models;
+using CKK.Logic.Interfaces;
+using CKK.DB.UOW;
+using CKK.DB.Interfaces;
+
+namespace CoreysKnickKnacksWPFForm
+{
+    public partial class AddItemWindow : Window
+    {
+
+        IConnectionFactory conn = new DatabaseConnectionFactory();
+        public UnitOfWork uow;
+        public AddItemWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (idTextBox != null && quantityTextBox != null && priceTextBox != null)
+            {
+                DialogResult = true;
+                Close();
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    
+    }
+    
+}
---
+++ CoreysKnickKnacks/Windows/RemoveItem.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CKK.Logic.Interfaces;
+using CKK.Logic.Models;
+using CKK.Logic.Exceptions;
+using CKK.DB.Interfaces;
+using CKK.DB.UOW;
+
+namespace CoreysKnickKnacksWPFForm
+{
+    /// <summary>
+    /// Interaction logic for RemoveItem.xaml
+    /// </summary>
+    public partial class RemoveItem : Window
+    {
+
+        IConnectionFactory conn;
+        UnitOfWork UOW;
+        Product comboBoxProd;
+        public RemoveItem()
+        {
+            InitializeComponent();
+            conn = new DatabaseConnectionFactory();
+            UOW = new UnitOfWork(conn);
+            removeItemComboBox.ItemsSource = UOW.Products.GetAll();
+        }
+
+        private void removeItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (removeItemComboBox.SelectedItem != null)
+            {
+                UOW.Products.Delete(comboBoxProd.Id);
+                DialogResult = true;
+                Close();
+            }
+
+        }
+
+        private void cancelRemoveItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        
+    }
+}
