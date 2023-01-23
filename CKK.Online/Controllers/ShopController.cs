90bb7ecb45e364824f4a42a33d559f690cfa3d1b
MIchael Ricks <michaelricks0019@gmail.com>
2023-01-23 12:03:36 -0700

    save

---
+++ CKK.Online/Controllers/ShopController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CKK.Logic.
+
+namespace CKK.Online.Controllers
+{
+    public class ShopController : Controller
+    {
+        public ShopController() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
